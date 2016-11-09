e655b592ed8edbb7bd5b85c90a4e763be48235d9
Vladimir Angelov <vladimir@siriushome.com>
2016-11-09 13:16:25 +0200

    Added DOMParser third-party package.

---
+++ Bridge.Html5.DOMParser/DOMParser.cs
@@ -0,0 +1,30 @@
+﻿namespace Bridge.Html5
+{
+	/// <summary>
+	/// DOMParser can parse XML or HTML source stored in a string into a DOM Document.
+	/// </summary>
+	[External]
+	[Namespace(false)]
+	[Name("DOMParser")]
+	public class DOMParser
+	{
+		/// <summary>
+		/// Constructs a new DOMParser object.
+		/// </summary>
+		public extern DOMParser();
+
+		/// <summary>
+		/// Builds an XMLDocument object from the specified string.
+		/// </summary>
+		/// <param name="xmlString">String that specifies the contents of the XML file to parse.</param>
+		/// <param name="mimeType">String that specifies the content type of the string to parse. Can be one of the following:
+		/// text/html
+		/// text/xml
+		/// application/xml
+		/// application/xhtml+xml
+		/// image/svg+xml
+		/// </param>
+		/// <returns></returns>
+		public extern DocumentInstance ParseFromString(string xmlString, string mimeType);
+	}
+}
---
+++ Bridge.Html5.DOMParser/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bridge.Html5.DOMParser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Bridge.Html5.DOMParser")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("353412dd-fc70-4ee4-9c02-9ee69d8f9cbe")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: AssemblyInformationalVersion("1.0")]
